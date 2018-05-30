7f0fb1da89a43b84c69e60c0dd10a9bb848c1005
SakuraNeko <NekoCN@outlook.com>
2018-05-30 12:57:30 +0800

    第一日

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            int a = 2;
+            Console.WriteLine(a);
+            a = 3;
+            Console.WriteLine(a);
+            a = a + 1;
+            Console.WriteLine(a);
+
+            string s1 = "hello";
+            string s2 = "World";
+            Console.WriteLine(s1 + s2);
+            Console.WriteLine(s1 + a);
+            Console.WriteLine("a=" + a);
+
+            a = 3;
+            int b = 0;
+            bool c = a == 4;
+
+            if(a == 3)
+            {
+                b = 4;
+            }
+            else if(a == 4)
+            {
+                b = 5;
+            }
+            else if(a == 5)
+            {
+                b = 6;
+            }
+            else
+            {
+                b = 100;
+            }
+            Console.WriteLine("b=" + b);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ RandonCard/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace RandonCard
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int r = random.Next(0,100);
+
+            string s = "";
+
+            if(r<10)//r>=0 && r<10
+            {
+                s = "关羽";
+            }
+            else if(r<30)//r>=10 && r<30
+            {
+                s = "张飞";
+            }
+            else if(r<60)//r>=10 && r<60
+            {
+                s = "赵云";
+            }
+            else if(r<100)//r>=60 && r<100
+            {
+                s = "黄忠";
+            }
+            Console.WriteLine("您抽到了卡牌 " + s +"(" + r + ")");
+            Console.ReadKey();
+        }
+    }
+}
